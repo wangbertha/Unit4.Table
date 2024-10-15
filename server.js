5d5c5cfb3c06e55354b242b657ecf9b4a963af24
wangbertha <wangbertha@gmail.com>
2024-10-15 11:47:48 -0500

    Create API endpoint and test for POST reservations

--- server.js
+++ server.js
@@ -2,10 +2,29 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+const prisma = require("./prisma");
+
 app.use(require("morgan")("dev"));
 app.use(express.json());
 
-// TODO: POST /reservations
+app.post("/reservations", async (req, res, next) => {
+  const { restaurantId, customerIds } = req.body;
+  try {
+    const reservation = await prisma.reservation.create({
+      data: {
+        date: new Date(Date.now()).toDateString(),
+        restaurantId: restaurantId,
+        party: { connect: customerIds.map((id) => ({ id })) },
+      },
+      include: {
+        party: true,
+      },
+    });
+    res.status(201).json(reservation);
+  } catch (e) {
+    next(e);
+  }
+})
 
 app.use((req, res, next) => {
   next({ status: 404, message: "Endpoint not found." });
